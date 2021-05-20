f5ee3a8e1113c7c0497c7e897348d10df817e759
8435 <843523836@qq.com>
2021-05-20 21:00:37 +0800

    init

---
+++ src/main/java/com/zhai/zhaizhai/ZhaizhaiApplication.java
@@ -0,0 +1,13 @@
+package com.zhai.zhaizhai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhaizhaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhaizhaiApplication.class, args);
+    }
+
+}

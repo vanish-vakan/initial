91d05f736b8ab07d2140429bbede5733b9c74157
8435 <843523836@qq.com>
2021-05-20 21:00:37 +0800

    init

---
+++ src/test/java/com/zhai/zhaizhai/ZhaizhaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhai.zhaizhai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZhaizhaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
